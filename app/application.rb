40370662801faa0ee1dec15956f9e5980fe082de
ERIC KELLY <eakelly220@gmail.com>
2018-06-28 20:26:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,7 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/search/)
+      binding.pry
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
