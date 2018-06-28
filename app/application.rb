2fe05ee5eaaa778ea19b0416141c61f3fd59eb3d
ERIC KELLY <eakelly220@gmail.com>
2018-06-28 19:51:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,15 +10,12 @@ class Application
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
-    end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    end
     elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
-    end
     else
       resp.write "Path Not Found"
     end
